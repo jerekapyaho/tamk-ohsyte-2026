404134a10d3195b3bc0931c8f7ef06231269b425
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2026-01-09 10:36:17 +0200

    Array of event tuples

--- 02/events/src/main.rs
+++ 02/events/src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
-    let description = "Rust 1.0.0 released";
-    let date = 2015_05_15;
-    println!("{}: {}", date, description);    
+    let events = [
+        (1996_01_23, "JDK 1.0 released"), // the first version of Java
+        (2008_12_03, "Python 3.0 released"), // the modern version of Python
+        (2015_05_15, "Rust 1.0.0 released"),
+    ];
+    println!("{:#?}", events);
+    println!("{} events", events.len());
+    println!("Most important event: {:?}", events[2]);
 }
