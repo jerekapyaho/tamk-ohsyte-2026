8c2aa38b80ff60bbebe2e975b5a13fc2ef19c61c
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2026-01-15 15:21:27 +0200

    Events example updated

--- 02/events/src/main.rs
+++ 02/events/src/main.rs
@@ -1,10 +1,46 @@
 fn main() {
     let events = [
-        (1996_01_23, "JDK 1.0 released"), // the first version of Java
-        (2008_12_03, "Python 3.0 released"), // the modern version of Python
+        (1996_01_23, "JDK 1.0 released"),
+        (2008_12_03, "Python 3.0 released"),
         (2015_05_15, "Rust 1.0.0 released"),
+        (2025_09_16, "Java 25 released"),
+        (2025_10_07, "Python 3.14 released"),
+        (2025_12_11, "Rust 1.92.0 released"),
     ];
-    println!("{:#?}", events);
-    println!("{} events", events.len());
-    println!("Most important event: {:?}", events[2]);
+
+    // Get the month and day from our target date:
+    let date = 2015_05_15;
+    let date_string = date.to_string();
+    let month_string = &date_string[4..6];
+    let month: i32 = month_string.parse().unwrap();
+    let day_string = &date_string[6..8];
+    let day: i32 = day_string.parse().unwrap();
+    let month_day = (month, day);
+
+    let mut any_luck = false;
+
+    for event in events {
+        // Get the month and day from the event's date:
+        let event_date_string = event.0.to_string();
+        let event_month_string = &event_date_string[4..6];
+        let event_month: i32 = event_month_string.parse().unwrap();
+        let event_day_string = &event_date_string[6..8];
+        let event_day: i32 = event_day_string.parse().unwrap();
+        let event_month_day = (event_month, event_day);
+
+        // Get the event's year separately for printing:
+        let year_string = &event_date_string[0..4];
+        let year: i32 = year_string.parse().unwrap();
+
+        // Compare the month-day pairs and print
+        // event year and description if equal:
+        if event_month_day == month_day {
+            println!("{}: {}", year, event.1);
+            any_luck = true;
+        }
+    }
+
+    if !any_luck {
+        println!("No events found.");
+    }
 }
