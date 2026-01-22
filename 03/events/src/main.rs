bc7b4e0d3416e20b75d8f39f4b8eda951058278b
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2026-01-22 14:32:53 +0200

    MonthDay

--- 03/events/src/main.rs
+++ 03/events/src/main.rs
@@ -4,79 +4,63 @@ struct Date {
     day: u8,
 }
 
+impl Date {
+    fn new(year: i16, month: u8, day: u8) -> Self {
+        Self { year, month, day }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct MonthDay {
+    month: u8,
+    day: u8,
+}
+
 struct Event {
     date: Date,
     description: String,
 }
 
+impl Event {
+    fn new(date: Date, description: String) -> Self {
+        Event { date, description }
+    }
+
+    fn month_day(&self) -> MonthDay {
+        MonthDay {
+            month: self.date.month,
+            day: self.date.day,
+        }
+    }
+}
+
 fn main() {
     let events = [
-        Event {
-            date: Date {
-                year: 1996,
-                month: 1,
-                day: 23,
-            },
-            description: String::from("JDK 1.0 released"),
-        },
-        Event {
-            date: Date {
-                year: 2008,
-                month: 12,
-                day: 3,
-            },
-            description: String::from("Python 3.0 released"),
-        },
-        Event {
-            date: Date {
-                year: 2015,
-                month: 5,
-                day: 15,
-            },
-            description: String::from("Rust 1.0.0 released"),
-        },
-        Event {
-            date: Date {
-                year: 2025,
-                month: 9,
-                day: 16,
-            },
-            description: String::from("Java 25 released"),
-        },
-        Event {
-            date: Date {
-                year: 2025,
-                month: 10,
-                day: 7,
-            },
-            description: String::from("Python 3.14 released"),
-        },
-        Event {
-            date: Date {
-                year: 2025,
-                month: 12,
-                day: 11,
-            },
-            description: String::from("Rust 1.92.0 released"),
-        },
+        Event::new(
+            Date::new(2025, 12, 11),
+            String::from("Rust 1.92.0 released"),
+        ),
+        Event::new(Date::new(1996, 1, 23), String::from("JDK 1.0 released")),
+        Event::new(Date::new(2008, 12, 3), String::from("Python 3.0 released")),
+        Event::new(Date::new(2015, 5, 15), String::from("Rust 1.0.0 released")),
+        Event::new(Date::new(2025, 9, 16), String::from("Java 25 released")),
+        Event::new(Date::new(2025, 10, 7), String::from("Python 3.14 released")),
+        Event::new(
+            Date::new(2025, 12, 11),
+            String::from("Rust 1.92.0 released"),
+        ),
     ];
 
-    // Get the month and day from our target date:
-    let date = Date { year: 2015, month: 5, day: 15 };
-
-    let month_day = (date.month, date.day);
-
-    let mut any_luck = false;
-
+    let month_day = MonthDay { month: 12, day: 3 };
+    let mut any_luck = false; // Boolean flag
     for event in events {
-        let event_month_day = (event.date.month, event.date.day);
-        if event_month_day == month_day {
+        if event.month_day() == month_day {
             println!("{}: {}", event.date.year, event.description);
             any_luck = true;
         }
     }
 
     if !any_luck {
-        println!("No events found.");
+        println!("No events for {:#?}", month_day);
     }
 }
