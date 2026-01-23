3a486dc4e0aab8d225c7d97eb247f862d97faa55
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2026-01-23 11:32:53 +0200

    Use enum for month

--- 03/events/src/main.rs
+++ 03/events/src/main.rs
@@ -1,18 +1,33 @@
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum Month {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December,
+}
 struct Date {
     year: i16,
-    month: u8,
+    month: Month,
     day: u8,
 }
 
 impl Date {
-    fn new(year: i16, month: u8, day: u8) -> Self {
+    fn new(year: i16, month: Month, day: u8) -> Self {
         Self { year, month, day }
     }
 }
 
 #[derive(Debug, PartialEq)]
 struct MonthDay {
-    month: u8,
+    month: Month,
     day: u8,
 }
 
@@ -37,21 +52,32 @@ impl Event {
 fn main() {
     let events = [
         Event::new(
-            Date::new(2025, 12, 11),
-            String::from("Rust 1.92.0 released"),
+            Date::new(1996, Month::January, 23),
+            String::from("JDK 1.0 released"),
         ),
-        Event::new(Date::new(1996, 1, 23), String::from("JDK 1.0 released")),
-        Event::new(Date::new(2008, 12, 3), String::from("Python 3.0 released")),
-        Event::new(Date::new(2015, 5, 15), String::from("Rust 1.0.0 released")),
-        Event::new(Date::new(2025, 9, 16), String::from("Java 25 released")),
-        Event::new(Date::new(2025, 10, 7), String::from("Python 3.14 released")),
         Event::new(
-            Date::new(2025, 12, 11),
+            Date::new(2008, Month::December, 3),
+            String::from("Python 3.0 released"),
+        ),
+        Event::new(
+            Date::new(2015, Month::May, 15),
+            String::from("Rust 1.0.0 released"),
+        ),
+        Event::new(
+            Date::new(2025, Month::September, 16),
+            String::from("Java 25 released"),
+        ),
+        Event::new(
+            Date::new(2025, Month::October, 7),
+            String::from("Python 3.14 released"),
+        ),
+        Event::new(
+            Date::new(2025, Month::December, 11),
             String::from("Rust 1.92.0 released"),
         ),
     ];
 
-    let month_day = MonthDay { month: 12, day: 3 };
+    let month_day = MonthDay { month: Month::May, day: 15 };
     let mut any_luck = false; // Boolean flag
     for event in events {
         if event.month_day() == month_day {
